e7bdc72398bec15b8cfc428dccfdfe436f7acd3a
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-11-10 00:19:04 -0300

    definition of variants

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,17 @@
-import * as React from "react";
+import * as React from 'react'
 import { MxButton } from 'mx-button'
 
 const App = () => {
-  return <MxButton label={"Mx Button"}/>
+  return (
+    <div>
+      <MxButton label={'Mx Button small'} variant='primary' />
+      <MxButton label={'Mx Button small'} variant='secundary' />
+      <MxButton label={'Mx Button small'} variant='outlined' />
+      <MxButton label={'Mx Button small'} variant='text' />
+      <MxButton label={'Mx Button small'} variant='rounded' />
+      <MxButton label={'Mx Button small'} variant='square' />
+    </div>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
 import * as React from 'react'
-import { Button as MuiButton, ButtonProps } from '@material-ui/core'
+import { Button as MuiButton, } from '@material-ui/core'
+import { createStyles, makeStyles, Theme } from '@material-ui/core'
 
-/**
- *
- */
-interface MxButtonProps extends ButtonProps {
-  /** Caption text of button */
+const useMxButtonStyles = makeStyles<Theme>(() =>
+  createStyles({
+    root: {
+      fontFamily: 'Gotham'
+    },
+  })
+)
+
+type Variant =
+  | 'primary'
+  | 'secundary'
+  | 'outlined'
+  | 'text'
+  | 'rounded'
+  | 'square'
+
+interface MxButtonProps {
   readonly label?: string
+  readonly variant: Variant;
 }
 
 /**
@@ -15,5 +29,42 @@ interface MxButtonProps extends ButtonProps {
  * @returns
  */
 export const MxButton: React.VFC<MxButtonProps> = (props) => {
-  return <MuiButton {...props}>{props.label ? `${props.label}` : ''}</MuiButton>
+  const classes = useMxButtonStyles()
+  
+  const classesPrimary = {
+    root : classes.root,
+  };
+  const classesSecundary = {
+    root : classes.root,
+  };
+  const classesOutlined = {
+    root : classes.root,
+  };
+  const classesText = {
+    root : classes.root,
+  };
+  const classesRounded ={
+    root : classes.root,
+  }
+  const classesSquare ={
+    root : classes.root,
+  }
+  
+  const selectVariant = (variant : Variant) => {
+    if(variant === "primary") return classesPrimary;
+    if(variant === "secundary") return classesSecundary;
+    if(variant === "outlined") return classesOutlined;
+    if(variant === "text") return classesText;
+    if(variant === "rounded") return classesRounded;
+    if(variant === "square") return classesSquare;    
+    return undefined;
+  }
+
+  return (
+    <MuiButton
+      classes={selectVariant(props.variant)}
+    >
+      {props.label ? `${props.label}` : ''}
+    </MuiButton>
+  )
 }
