4ecb9dbd8ac748c7a4adff361f942ccb1bd10d4a
Maximiliano Correa <jmaxicorrea@gmail.com>
2022-11-12 02:43:36 -0300

    Rounded and Square Variants

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,13 +3,84 @@ import { MxButton } from 'mx-button'
 
 const App = () => {
   return (
-    <div>
-      <MxButton label={'Mx Button small'} variant='primary' />
-      <MxButton label={'Mx Button small'} variant='secundary' />
-      <MxButton label={'Mx Button small'} variant='outlined' />
-      <MxButton label={'Mx Button small'} variant='text' />
-      <MxButton label={'Mx Button small'} variant='rounded' />
-      <MxButton label={'Mx Button small'} variant='square' />
+    <div style={{ display: 'flex', margin: '20px', flexDirection: 'column' }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <span style={{width : "100px"}}>Primary</span>
+        <MxButton label={'Mx Button small'} variant='primary' size='small'/>
+        <MxButton label={'Mx Button medium'} variant='primary' size='medium'/>
+        <MxButton label={'Mx Button large'} variant='primary' size='large'/>
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <span style={{width : "100px"}}>Secundary</span>
+        <MxButton label={'Mx Button small'} variant='secundary' size='small'/>
+        <MxButton label={'Mx Button medium'} variant='secundary' size='medium'/>
+        <MxButton label={'Mx Button large'} variant='secundary' size='large'/>
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <span style={{width : "100px"}}>Outlined</span>
+        <MxButton label={'Mx Button small'} variant='outlined' size='small'/>
+        <MxButton label={'Mx Button medium'} variant='outlined' size='medium'/>
+        <MxButton label={'Mx Button large'} variant='outlined' size='large'/>
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <span style={{width : "100px"}}>Text</span>
+        <MxButton label={'Mx Button small'} variant='text' size='small'/>
+        <MxButton label={'Mx Button medium'} variant='text' size='medium'/>
+        <MxButton label={'Mx Button large'} variant='text' size='large'/>
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <span style={{width : "100px"}}>Rounded</span>
+        <MxButton label={'Mx Button small'} variant='rounded' size='small'/>
+        <MxButton label={'Mx Button medium'} variant='rounded' size='medium'/>
+        <MxButton label={'Mx Button large'} variant='rounded' size='large'/>
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <span style={{width : "100px"}}>Square</span>
+        <MxButton label={'Mx Button small'} variant='square' size='small'/>
+        <MxButton label={'Mx Button medium'} variant='square' size='medium'/>
+        <MxButton label={'Mx Button large'} variant='square' size='large'/>
+      </div>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react'
-import { Button as MuiButton, } from '@material-ui/core'
+import { Button as MuiButton } from '@material-ui/core'
 import { createStyles, makeStyles, Theme } from '@material-ui/core'
 
 const useMxButtonStyles = makeStyles<Theme>(() =>
   createStyles({
-    root: {
-      fontFamily: 'Gotham'
+    rootDefault: {
+      fontFamily: 'Gotham',
+      borderRadius: '4px'
+    },
+    rootRounded: {
+      fontFamily: 'Gotham',
+      borderRadius: '30px'
+    },
+    rootSquare: {
+      fontFamily: 'Gotham',
+      borderRadius: '0px'
     },
   })
 )
+
+type Size = 'small' | 'medium' | 'large'
 
 type Variant =
   | 'primary'
@@ -20,7 +31,8 @@ type Variant =
 
 interface MxButtonProps {
   readonly label?: string
-  readonly variant: Variant;
+  readonly variant: Variant
+  readonly size: Size
 }
 
 /**
@@ -30,39 +42,19 @@ interface MxButtonProps {
  */
 export const MxButton: React.VFC<MxButtonProps> = (props) => {
   const classes = useMxButtonStyles()
-  
-  const classesPrimary = {
-    root : classes.root,
-  };
-  const classesSecundary = {
-    root : classes.root,
-  };
-  const classesOutlined = {
-    root : classes.root,
-  };
-  const classesText = {
-    root : classes.root,
-  };
-  const classesRounded ={
-    root : classes.root,
-  }
-  const classesSquare ={
-    root : classes.root,
-  }
-  
-  const selectVariant = (variant : Variant) => {
-    if(variant === "primary") return classesPrimary;
-    if(variant === "secundary") return classesSecundary;
-    if(variant === "outlined") return classesOutlined;
-    if(variant === "text") return classesText;
-    if(variant === "rounded") return classesRounded;
-    if(variant === "square") return classesSquare;    
-    return undefined;
-  }
 
   return (
     <MuiButton
-      classes={selectVariant(props.variant)}
+      classes={{
+        root:
+          props.variant === 'rounded'
+            ? classes.rootRounded
+            : props.variant === 'square'
+            ? classes.rootSquare
+            : classes.rootDefault,
+
+        label: undefined
+      }}
     >
       {props.label ? `${props.label}` : ''}
     </MuiButton>
